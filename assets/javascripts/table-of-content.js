1a9bf4d871e703190b5367ee753e2242ff085018
Yann Klis <yann.klis@novelys.com>
2015-09-19 09:43:19 +0200

    better table of content

--- assets/javascripts/table-of-content.js
+++ assets/javascripts/table-of-content.js
@@ -9,7 +9,7 @@ define(['jquery'], function ($) {
 
   if (window.location.pathname !== '/404.html' && document.title.indexOf('404 Not found') == -1) {
     var sidebar_nav = $('.sidebar-nav')
-    var headers = $('.content').find(':header:not(h1)')
+    var headers = $('.content').find('h2')
     var ul = $('<ul>').addClass('list-unstyled')
 
     headers.each(function (index, el) {
@@ -23,6 +23,10 @@ define(['jquery'], function ($) {
       var a = $('<a>')
         .attr('href','#' + id)
         .text(innerText)
+
+      a.on("click", function(e){
+        $('html, body').animate( { scrollTop: $("#"+id).offset().top }, 600 )
+      })
 
       var li = $('<li>')
         .append(a)
