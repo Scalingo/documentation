c0cda40d35b147ca905bc4b53c3955be375ad0a1
William Hollacsek <william.hollacsek@gmail.com>
2016-01-14 11:34:01 +0100

    Algolia docsearch integration

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,23 +1,18 @@
 requirejs.config({
   waitSeconds: 200,
   paths: {
-    'jquery': '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min'
+    'jquery': '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
+    'docsearch': '//cdn.jsdelivr.net/docsearch.js/1/docsearch.min'
   },
   shim: {
-    'bootstrap-modal': ['jquery'],
-    'jquery.swiftype.autocomplete': ['jquery'],
-    'jquery.swiftype.search': ['jquery'],
-    'search': ['jquery', 'jquery.swiftype.autocomplete', 'jquery.swiftype.search', 'bootstrap-modal'],
-    'jquery.ba-hashchange.min': ['jquery'],
+    'search': ['docsearch'],
     'highlight': ['highlight.pack'],
     'anchor': ['jquery', 'table-of-content']
   }
 })
 
 requirejs([
-  'table-of-content',
-  'jquery.ba-hashchange.min',
   'highlight',
   'search',
   'anchor'
-  ])
+])
