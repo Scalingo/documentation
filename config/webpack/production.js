129808cb71d99a1a3492ec1a83e7cb1679cf1f40
Yann Klis <yann.klis@gmail.com>
2018-02-21 16:33:15 +0100

    output production env

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,7 +2,9 @@ const environment = require('./environment')
 
 console.log("********************************************")
 console.log("********************************************")
-console.log("********************************************")
+console.log(JSON.stringify(process.env))
+console.log("++++++++++++++++++++++++++++++++++++++++++++")
+console.log("++++++++++++++++++++++++++++++++++++++++++++")
 console.log(JSON.stringify(environment.toWebpackConfig()))
 
 module.exports = environment.toWebpackConfig()
