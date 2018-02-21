af49eac6fbc6e8c5004effeebc5b7593346db6a3
Yann Klis <yann.klis@gmail.com>
2018-02-21 17:51:26 +0100

    more output

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,10 +1,28 @@
 const environment = require('./environment')
 
-console.log("********************************************")
-console.log("********************************************")
-console.log(JSON.stringify(process.env))
 console.log("++++++++++++++++++++++++++++++++++++++++++++")
+
+const { config } = require('@rails/webpacker')
+const {
+  basename, dirname, join, relative, resolve
+} = require('path')
+const { sync } = require('glob')
+
+const getExtensionsGlob = () => {
+  const { extensions } = config
+  return extensions.length === 1 ? `**/*${extensions[0]}` : `**/*{${extensions.join(',')}}`
+}
+
+const glob = getExtensionsGlob()
+
+console.log(config.source_path)
+console.log(config.source_entry_path)
+const rootPath = join(config.source_path, config.source_entry_path)
+console.log(rootPath)
+const paths = sync(join(rootPath, glob))
+console.log(paths)
+
+
 console.log("++++++++++++++++++++++++++++++++++++++++++++")
-console.log(JSON.stringify(environment.toWebpackConfig()))
 
 module.exports = environment.toWebpackConfig()
