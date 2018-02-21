034220d93a6aeee6b14c10770866980898f5045a
Yann Klis <yann.klis@gmail.com>
2018-02-21 18:05:54 +0100

    fuck you

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,35 +1,3 @@
 const environment = require('./environment')
 
-console.log("++++++++++++++++++++++++++++++++++++++++++++")
-const fs = require('fs')
-const { config } = require('@rails/webpacker')
-const {
-  basename, dirname, join, relative, resolve
-} = require('path')
-const { sync } = require('glob')
-
-const getExtensionsGlob = () => {
-  const { extensions } = config
-  return extensions.length === 1 ? `**/*${extensions[0]}` : `**/*{${extensions.join(',')}}`
-}
-
-const glob = getExtensionsGlob()
-
-console.log(config.source_path)
-console.log(config.source_entry_path)
-const rootPath = join(config.source_path, config.source_entry_path)
-console.log(rootPath)
-console.log(join(rootPath, glob))
-const paths = sync(join(rootPath, glob))
-console.log(paths)
-
-console.log("********************")
-console.log(process.cwd())
-console.log(fs.readdirSync("."))
-console.log(fs.readdirSync("app"))
-console.log(fs.readdirSync("app/javascript"))
-console.log(fs.readdirSync("app/javascript/packs"))
-
-console.log("++++++++++++++++++++++++++++++++++++++++++++")
-
 module.exports = environment.toWebpackConfig()
