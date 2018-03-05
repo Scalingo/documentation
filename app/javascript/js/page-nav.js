b2c27c67e05374e59aea394630bdef6b322cb003
Yann Klis <yann.klis@gmail.com>
2018-03-05 16:37:19 +0100

    site-nav and breadcrumb

--- app/javascript/js/page-nav.js
+++ app/javascript/js/page-nav.js
@@ -1,24 +1,26 @@
 require('scrollspy-js')
+const scrollToElement = require('scroll-to-element')
 
-var article = document.getElementById('article')
-if (article !== null) {
-  var spy = new ScrollSpy('#article', {
-    nav: '.page-nav li > a',
-    className: 'active'
-  })
-}
+document.addEventListener("DOMContentLoaded", function(event) {
+  var article = document.getElementById('article')
+  if (article !== null) {
+    var spy = new ScrollSpy('#article', {
+      nav: '.page-nav li > a',
+      className: 'active'
+    })
+  }
 
-var scrollToElement = require('scroll-to-element')
-
-document.querySelectorAll('.page-nav a').forEach((el) => {
-  el.addEventListener('click', function(e) {
-    e.preventDefault()
-    let node = e.target
-    var targetId = node.getAttribute('href')
-    scrollToElement(targetId, {
-      offset: -64,
-      ease: 'in-out-quad',
-      duration: 1000
+  document.querySelectorAll('.page-nav a').forEach((el) => {
+    el.addEventListener('click', function(e) {
+      e.preventDefault()
+      let node = e.target
+      var targetId = node.getAttribute('href')
+      location.hash = targetId
+      scrollToElement(targetId, {
+        offset: -64,
+        ease: 'in-out-quad',
+        duration: 500
+      })
     })
   })
 })
--- app/javascript/js/site-nav.js
+++ app/javascript/js/site-nav.js
@@ -1,14 +1,41 @@
-let navTitles = document.querySelectorAll('.nav-title')
-navTitles.forEach((element) => {
-  element.addEventListener('click', (e) => {
-    e.preventDefault()
-    let node = e.target
-    let parent = node.parentElement
-    let currentState = parent.getAttribute('data-state')
-    if (currentState == 'open') {
-      parent.setAttribute('data-state', 'closed')
-    } else {
-      parent.setAttribute('data-state', 'open')
-    }
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  let navTitles = document.querySelectorAll('.nav-title')
+  navTitles.forEach((element) => {
+    element.addEventListener('click', (e) => {
+      e.preventDefault()
+      let node = e.target
+      let parent = node.parentElement
+      let currentState = parent.getAttribute('data-state')
+      if (currentState == 'open') {
+        parent.setAttribute('data-state', 'closed')
+      } else {
+        parent.setAttribute('data-state', 'open')
+      }
+    })
   })
+
+  function computeSiteNavHeight(siteNav) {
+    let rect = siteNav.getBoundingClientRect()
+    let top = rect.top
+    let height = 200
+    if (top >= 94) {
+      height = window.innerHeight - top
+    } else {
+      height = rect.bottom - 94
+    }
+    siteNav.style.height = height - 20 + "px"
+  }
+
+  let siteNav = document.querySelector('.site-nav > nav')
+  if (siteNav !== null) {
+    window.addEventListener('scroll', function(ev) {
+      computeSiteNavHeight(siteNav)
+    })
+    window.addEventListener('resize', function(ev) {
+      computeSiteNavHeight(siteNav)
+    })
+  }
+  computeSiteNavHeight(siteNav)
+
 })
