7719e70e35e7812ff3c6a4e59f33de17009df879
Jean Bertrand <jean@scalingo.com>
2022-01-17 16:51:01 +0100

    fix: issue on macos and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
     "./_includes/**/*.{html,erb,md,mdown}",
-    "./layouts/**/*.{html,erb,md,mdown}",
-    "./posts/**/*.{html,erb,md,mdown}",
+    "./_layouts/**/*.{html,erb,md,mdown}",
+    "./_posts/**/*.{html,erb,md,mdown}",
     "./app/**/*.{html,erb,md,mdown}",
     "./changelog/**/*.{html,erb,md,mdown}",
   ],
