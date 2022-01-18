4b4869b1451d5110f275bdcc4bcaf7d34f2bf054
cyrille <cyrille@scalingo.com>
2022-01-18 11:28:38 +0100

    design system and project init

---
+++ design_system.md
@@ -0,0 +1,32 @@
+---
+layout: default
+---
+
+<div class="mx-64">
+# Titles
+
+<h1>Title H1</h1>
+<h2>Title H2</h2>
+<h3>Title H3</h3>
+<h4>Title H2</h4>
+<h5>Title H2</h5>
+
+# Paragraph Elements
+
+<p>Paragraph</p>
+
+<ul>
+  <li>Element 1</li>
+  <li>Element 2</li>
+</ul>
+
+<ol>
+  <li>Element 1</li>
+  <li>Element 2</li>
+</ol>
+
+<hr/> 
+
+{% include button.html text="My text" link="scalingo.com" title="Link to homepage" %}
+
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,74 @@ module.exports = {
     "./_includes/**/*.{html,erb,md,mdown}",
     "./_layouts/**/*.{html,erb,md,mdown}",
     "./_posts/**/*.{html,erb,md,mdown}",
-    "./app/**/*.{html,erb,md,mdown}",
+    "./app/**/*.{html,erb,md,mdown,css}",
     "./changelog/**/*.{html,erb,md,mdown}",
+    "./*.{html,erb,md,mdown}"
   ],
-  theme: {},
+  theme: {
+    fontFamily: {
+      sans: ["aktiv-grotesk"],
+      serif: ["freight-text-pro"],
+    },
+    colors: {
+      "sc-gray-1": "#1F2933",
+      "sc-gray-2": "#7B8794",
+      "sc-gray-3": "#E5E7EA",
+      "sc-gray-4": "#F8F9FA",
+      "sc-gray-5": "#FFFFFF",
+      "sc-blue-1": "#183BEE",
+      "sc-blue-2": "#D1D8FC",
+      "sc-blue-3": "#F8F9FF",
+      "sc-turquoise-1": "#22D1EE",
+      "sc-turquoise-2": "#A7EDF8",
+      "sc-turquoise-3": "#E9FAFD",
+      "sc-yellow-1": "#FFD600",
+      "sc-yellow-2": "#FFD600",
+      "sc-yellow-3": "#FFFBE5",
+    },
+    fontSize: {
+      "sc-text-1": [
+        "18px",
+        {
+          lineHeight: "38px"
+        },
+      ],
+      "sc-title-1": [
+        "32px",
+        {
+          lineHeight: "28px",
+          letterSpacing: "-0.04em",
+        },
+      ],
+      "sc-title-2": [
+        "24px",
+        {
+          lineHeight: "28px",
+          letterSpacing: "-0.04em",
+        },
+      ],
+      "sc-title-3": [
+        "22px",
+        {
+          lineHeight: "28px",
+          letterSpacing: "-0.04em",
+        },
+      ],
+      "sc-title-4": [
+        "20px",
+        {
+          lineHeight: "28px",
+          letterSpacing: "-0.04em",
+        },
+      ],
+      "sc-title-5": [
+        "18px",
+        {
+          lineHeight: "28px",
+          letterSpacing: "-0.04em",
+        },
+      ],
+    }
+  },
   plugins: [],
 };
