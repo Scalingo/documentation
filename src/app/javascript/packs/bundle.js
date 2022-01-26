4fb0ce86e164a0ce50cac0e6f89eaadfd85588cd
Jean Bertrand <jean@scalingo.com>
2022-01-26 16:24:44 +0100

    feat: Replace prod url with local/staging/review apps url

--- src/app/javascript/packs/bundle.js
+++ src/app/javascript/packs/bundle.js
@@ -9,16 +9,27 @@
 
 import docsearch from '@docsearch/js';
 
-docsearch({
-  container: '#docsearch-nav',
-  apiKey: '6967a402f012b8a7c4122180616aace0',
-  indexName: 'scalingo-doc',
-  placeholder: 'Search',
-});
+// Replace prod url with local/staging/review apps url
+function replaceUrl(url) {
+  var location = new URL(window.location);
+  var newBaseURL = `${location.protocol}//${location.host}/`;
+  var prodBaseURL = 'https://doc.scalingo.com/';
+  var new_url = url.replace(prodBaseURL, newBaseURL);
 
-docsearch({
-  container: '#docsearch-index',
-  apiKey: '6967a402f012b8a7c4122180616aace0',
-  indexName: 'scalingo-doc',
-  placeholder: 'Search',
-});
+  return new_url;
+}
+
+["#docsearch-nav", "#docsearch-index"].forEach((container) => {
+  docsearch({
+    container: container,
+    apiKey: '6967a402f012b8a7c4122180616aace0',
+    indexName: 'scalingo-doc',
+    placeholder: 'Search',
+    transformItems(items) {
+      return items.map((item) => {
+        item.url = replaceUrl(item.url);
+        return item;
+      });
+    },
+  });
+})
