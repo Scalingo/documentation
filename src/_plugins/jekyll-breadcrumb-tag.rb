71843a941df0736bd86414f63d2657f715f7d71f
Kevin Soltysiak <kevin@scalingo.com>
2022-03-02 14:13:33 +0100

    chore: bump linter and fix offenses

--- src/_plugins/jekyll-breadcrumb-tag.rb
+++ src/_plugins/jekyll-breadcrumb-tag.rb
@@ -6,12 +6,11 @@ module Jekyll
 
     def render(context)
       breadcrumb_hash = Jekyll.sites.first.data["breadcrumb_hash"]
-      current_url = context['page']['url']
+      current_url = context["page"]["url"]
       # Remove first and last element
       # First is blank and last is the current page
       url_parts = current_url.split("/")[1..-2]
 
-      result = []
       new_path = ""
 
       url_parts.map do |part|
