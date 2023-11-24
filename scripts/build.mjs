e3f33073d76a60c31c9c6a22573776845350590d
Kevin Soltysiak <kevin@scalingo.com>
2023-11-24 14:43:03 +0100

    chore: esbuild script

---
+++ scripts/build.mjs
@@ -0,0 +1,45 @@
+import * as esbuild from "esbuild";
+import postCssPlugin from 'esbuild-style-plugin'
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import postcssImport from 'postcss-import';
+import fs from 'fs';
+
+/**
+ * @type esbuild.BuildOptions
+ */
+const opts = {
+  entryPoints: [
+    "assets/application.js",
+    "assets/style.css",
+  ],
+  entryNames: '[dir]/[name]-[hash]',
+  bundle: true,
+  minify: true,
+  target: ["es2017"],
+  outdir: "src/assets",
+  plugins: [
+    postCssPlugin({
+      postcss: {
+        plugins: [tailwindcss, autoprefixer, postcssImport]
+      },
+    }),
+  ],
+  metafile: true,
+  logLevel: "info"
+};
+
+const lastOpt = process.argv[process.argv.length - 1];
+
+if(lastOpt === "--watch") {
+  // This is local development
+  opts.sourcemap = true;
+
+  let ctx = await esbuild.context(opts);
+
+  await ctx.watch();
+} else {
+  let result = await esbuild.build(opts);
+
+  fs.writeFileSync('assets/manifest.json', JSON.stringify(result.metafile, null, 2))
+}
