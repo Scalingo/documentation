c9cbc97a6ecdd1f86b8b53ce500018da3d098c7a
Kevin Soltysiak <kevin@scalingo.com>
2023-11-24 14:39:24 +0100

    chore: remove webpack(er)

--- config/webpack/development.js
+++
@@ -1,8 +0,0 @@
-const environment = require('./environment')
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
-environment.config.set('output.filename', '[name].js')
-
-environment.plugins.append('ExtractText2', new ExtractTextPlugin('[name].css'))
-
-module.exports = environment.toWebpackConfig()
