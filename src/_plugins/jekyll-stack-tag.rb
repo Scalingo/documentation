a14a144ca10988f407586e9fa264db15741d7f08
François KUBLER <francois+github@kubler.org>
2026-06-05 11:51:40 +0200

    feat(stacks): introduce new Jekyll tag for stacks

---
+++ src/_plugins/jekyll-stack-tag.rb
@@ -0,0 +1,25 @@
+module Jekyll
+  class StackTag < Liquid::Tag
+    CLASSES = {
+      "26" => "supported",
+      "24" => "default",
+      "22" => "deprecated",
+      "20" => "discontinued",
+      "18" => "discontinued",
+      "14" => "discontinued"
+    }.freeze
+
+    def initialize(tag_name, markup, tokens)
+      super
+      @stack = markup.strip
+    end
+
+    def render(context)
+      css_class = CLASSES.fetch(@stack, "unknown")
+
+      %(<span class="stack #{css_class}" title="#{css_class.capitalize}">scalingo-#{@stack}</span>)
+    end
+  end
+end
+
+Liquid::Template.register_tag("scalingo", Jekyll::StackTag)
